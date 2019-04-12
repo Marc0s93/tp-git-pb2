69a8ee85158d3283a9fe376e8697c3ef29fead16
Mell <mellmendez4089@gmail.com>
2019-04-12 16:48:22 -0300

    En ramaMelisa, se agregan las funcionalidades potencia y raiz a la calculadora

--- Ahorcado.java
+++ Ahorcado.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
+import java.lang.Math;	
 
 public class Ahorcado {
+
 	public static void main(String[] args) {
 		Integer a;
 		Integer b;
 		Integer opcion;
 		Integer resultado;
 		do{
-			System.out.println("Ingrese su operación /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
+			System.out.println("Ingrese su operación /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir/n 5 para potencia, de a elevado a la b /n 6 para raiz /n");
 			opcion = teclado.next();
-		}while(opcion < 1 && opcion > 4);
+		}while(opcion < 1 && opcion > 5);
 		System.out.println("Ingrese el primer numero");
-		a = teclado.next();
+			a = teclado.next();
 		System.out.println("Ingrese el segundo numero");
-		b = teclado.next();
+			b = teclado.next();
 
 		switch (opcion){
 		case 3: 
@@ -22,13 +24,19 @@ public class Ahorcado {
 		case 2: 
 			resultado = a - b;
 		break;
-
 		case 1:
 			resultado = a * b;
 		break;
-		case 4: resultado = a / b;
+		case 4: 
+			resultado = a / b;
 		break;
-                default: 
+		case 5: 
+			resultado = Math.pow(a, b); 
+		break;
+		case 6: 
+			resultado = Math.sqrt(x));
+
+	              default: 
 		System.out.println("OPCION INVALIDA");
 				break;
 		
