7ccf93ad5b37e089cccfbb4535f4e9b166be915e
Gaston <gastons525@gmail.com>
2019-04-11 23:10:43 -0300

    NADA

--- Ahorcado.java
+++ Ahorcado.java
@@ -4,29 +4,36 @@ public class Ahorcado {
 	public static void main(String[] args) {
 		Integer a;
 		Integer b;
+		
 		Integer opcion;
 		Integer resultado;
+		Scanner teclado = new Scanner (System.in);
+		
 		do{
 			System.out.println("Ingrese su operación /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
-			opcion = teclado.next();
+			opcion = teclado.nextInt();
 		}while(opcion < 1 && opcion > 4);
 		System.out.println("Ingrese el primer numero");
-		a = teclado.next();
+		a = teclado.nextInt();
 		System.out.println("Ingrese el segundo numero");
-		b = teclado.next();
+		b = teclado.nextInt();
 
 		switch (opcion){
 		case 3: 
 			resultado = a + b;
+			System.out.println("La suma es "+ resultado);
 		break;
 		case 2: 
 			resultado = a - b;
+			System.out.println("La resta es "+ resultado);
 		break;
 
 		case 1:
 			resultado = a * b;
+                        System.out.println("La Multiplicacion es "+ resultado);
 		break;
 		case 4: resultado = a / b;
+		System.out.println("La Division es "+ resultado);
 		break;
                 default: 
 		System.out.println("OPCION INVALIDA");
@@ -34,4 +41,5 @@ public class Ahorcado {
 		
 		}
 
+   }
 }
